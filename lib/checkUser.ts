1650236dcc7de8011f6bdf79f17b5ae3d141fb28
bestkavindu <bestkavindu@gmail.com>
2025-10-23 09:35:07 +0530

    Integrate Clerk authentication, add Navbar component, and update dependencies

---
+++ lib/checkUser.ts
@@ -0,0 +1,24 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { db } from "./db";
+
+export const checkUser = async () => {
+    const user = await currentUser();
+    if (!user) {
+        throw new Error("User not authenticated");
+    }
+    const loggedInUser = await db.user.findUnique({
+        where: { clerkUserId: user.id },
+    });
+    if (loggedInUser) {
+        return loggedInUser;
+    }
+
+    const newUser = await db.user.create({
+        data: {
+            clerkUserId: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            email: user.emailAddresses[0]?.emailAddress,
+        },
+    });
+    return newUser;
+}
